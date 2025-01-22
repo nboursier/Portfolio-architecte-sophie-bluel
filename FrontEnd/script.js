f8eaa1d2ef6bfe32141e8220ddad0dc009fc1161
nboursier <nicolasboursier@yahoo.fr>
2025-01-22 15:58:07 +0100

    modal ok

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -104,3 +104,44 @@ async function init() {
 init()
 
 
+async function initmodal() {
+    const data = await getworks()
+    console.log(data)
+    let j = data.length
+    
+    
+    for (let c = 0; c < j; c++) {
+        
+        const data2= data[c].imageUrl
+    const photomodal = document.getElementById("photomodal")
+    photomodal.classList.add("modal--window__containerproject")
+    const imagediv = document.createElement("figure")
+    imagediv.classList.add("modal--window__img")
+    const image = document.createElement("img")
+    const trashcontainer = document.createElement("div")
+    trashcontainer.classList.add("modal--window__trashiconcontainer")
+    image.classList.add("modal--window__img2")
+    const trashicon = document.createElement("svg")
+    trashicon.innerHTML = '<svg class="modal--window__trashicon"  id="trashicon"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z"/></svg>'
+    trashicon.classList.add("modal--window__trashicon")
+    image.src = data2
+    trashcontainer.appendChild(trashicon)
+    imagediv.appendChild(trashcontainer)
+    imagediv.appendChild(image)
+    photomodal.appendChild(imagediv)
+    }
+
+   
+}
+    
+
+
+initmodal()
+
+const dialog = document.querySelector("dialog");
+const showButton = document.getElementById("dialogopen");
+const closeButton = document.getElementById("dialogclose");
+showButton.addEventListener("click", () => {
+    dialog.showModal()})
+closeButton.addEventListener("click", () => {
+        dialog.close()})
--- FrontEnd/script2.js
+++ FrontEnd/script2.js
@@ -2,7 +2,7 @@
 
 // login //
 
-
+// "email": "sophie.bluel@test.tld" , "password" : "S0phie"
 
  
 //   {"email": "sophie.bluel@test.tld" ,"password": "S0phie" }
@@ -11,52 +11,67 @@
 //     "password": "string"
 //   }
 
-// const formu = document.getElementById("form")    
-// const fomrdata = new FormData(formu)
-// = Object.fromEntries(fomrdata)
-
 
 async function login() { 
-    console.log("salut")
-    let dataform = JSON.stringify({
-            "email": "sophie.bluel@test.tld" , "password" : "S0phie"
-        })
-    
+    const formu = document.getElementById("form")    
+    const fomrdata = new FormData(formu)
+    const dataf = Object.fromEntries(fomrdata)
+
+
+    let dataform = JSON.stringify(dataf)
+    console.log(dataform)
     const response1 = await fetch("http://localhost:5678/api/users/login",{
         method: "POST",
-        body:dataform
+        body:dataform,
+        headers:{
+            "Content-Type":"application/json"
+        }
     });
-    const data = await response1.json()
-    console.log(data)
-    return data
+    let datar = await response1.json()
+    
+    localStorage.setItem('Auth',datar)
+    console.log(datar)
+    
+    return datar
+    
+    
     
      
 }
 
-login()
 
-// formu.addEventListener("submit", (event) => {
-//     let baliseEmail = document.getElementById("email")
-//     let email = baliseEmail.value
 
-//     let baliseMdp = document.getElementById("password")
-//     let mdp = baliseMdp.value
-//     console.log(email)
-//     console.log(mdp)
-//     let regex = new RegExp("^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+$");
-//     let resultat = regex.test(email);
-//     if (email === "") {
-//         console.log('Le champ email est rempli');
-//     } else {
-//         console.log('Le champ email est vide');
-//     }
 
-//     if (resultat = true) {
-//         console.log('Le champ email est conforme');
-//     } else {
-//         console.log('Le champ email est non conforme');
-//     }
-//     event.preventDefault()
-//     console.log(data)
+form.addEventListener("submit", (event) => {
+    let baliseEmail = document.getElementById("email")
+    let email = baliseEmail.value
+
+    let baliseMdp = document.getElementById("mdp")
+    let mdp = baliseMdp.value
+    console.log(email)
+    console.log(mdp)
+    let regex = new RegExp("^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+$");
+    let resultat = regex.test(email);
+    if (email === "") {
+        console.log('Le champ email est vide');
+    } else {
+        console.log('Le champ email est rempli');
+    }
+
+    if (resultat = true) {
+        console.log('Le champ email est conforme');
+    } else {
+        console.log('Le champ email est non conforme');
+    }
+    event.preventDefault()
     
-// })
+    login()
+
+   
+})
+
+const tokeni= localStorage.getItem('Auth')
+
+console.log( tokeni)
+
+// 
